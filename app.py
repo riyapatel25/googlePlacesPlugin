3e92ed39b52b51ca3bd5c67414afc94a5a4d2f92
riyapatel25 <riyap7521@gmail.com>
2023-04-15 20:21:14 -0700

    added new endpoints

--- app.py
+++ app.py
@@ -7,8 +7,43 @@ app = Flask(__name__)
 
 api_key = os.environ.get("GOOGLE_API_KEY")
 
+def get_place_address(place_id):
+    # Construct the URL for the Place Details request
+    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=formatted_address&key={api_key}"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        address = data.get("result", {}).get("formatted_address", "")
+        return address
+    else:
+        return None
 
-def get_reviews(place_id):
+def get_lat_lng_from_address(address):
+    params = {
+        "address": address,
+    }
+    url = (
+        f"https://maps.googleapis.com/maps/api/geocode/json"
+        f"?address={params['address']}"
+        f"&key={api_key}"
+    )
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        location = (
+            response.json()
+            .get("results", [])[0]
+            .get("geometry", {})
+            .get("location", {})
+        )
+        return f"{location.get('lat')},{location.get('lng')}"
+    else:
+        return "1625 Leavenworth st apt 303 San Francisco CA 94109"
+
+
+def get_reviews(place_id, name, address):
     params = {
         "place_id": place_id,
     }
@@ -19,31 +54,42 @@ def get_reviews(place_id):
     )
     response = requests.get(url)
     if response.status_code == 200:
-        reviewArray = (
-            response.json().get("result", {}).get("reviews", [])
-        )  # Parse the JSON response
-        return reviewArray
+        reviewObj = {
+            "name": name,
+            "reviews": response.json().get("result", {}).get("reviews", []),
+            "address": address,
+        }
+        return reviewObj
     else:
-        return []
+        return {}
 
 
-@app.route("/get_place_details/<location>/<int:radius>/<type_of>", methods=["GET"])
-def proccessPlaces(location, radius, type_of):
+@app.route("/get_place_details/<address>/<int:radius>/<type_of>", methods=["GET"])
+def proccessPlaces(address, radius, type_of):
+    location = get_lat_lng_from_address(address)
+
+    if not location:
+        return {"error": "Unable to get coordinates for the specified address"}, 400
+
     params = {"key": api_key, "location": location, "radius": radius, "type": type_of}
+
     url = (
         f"https://maps.googleapis.com/maps/api/place/nearbysearch/json"
         f"?location={params['location']}"
         f"&radius={params['radius']}"
         f"&type={params['type']}"
         f"&key={api_key}"
+        f"&opennow=true"
     )
     response = requests.get(url)
 
     if response.status_code == 200:
         possibleNearbyPlaces = response.json().get("results", [])
         all_reviews = []
-        for place in possibleNearbyPlaces:
-            reviews = get_reviews(place['place_id'])
+        for index, place in enumerate(possibleNearbyPlaces):
+            name = place.get("name", "")
+            address = get_place_address(place["place_id"])
+            reviews = get_reviews(place["place_id"], name, address), 
             all_reviews.append(reviews)
         return all_reviews
     else:
@@ -52,3 +98,26 @@ def proccessPlaces(location, radius, type_of):
 
 if __name__ == "__main__":
     app.run(debug=False)
+
+# @app.route("/get_place_details/<location>/<int:radius>/<type_of>", methods=["GET"])
+# def proccessPlaces(location, radius, type_of):
+#     params = {"key": api_key, "location": location, "radius": radius, "type": type_of}
+#     url = (
+#         f"https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+#         f"?location={params['location']}"
+#         f"&radius={params['radius']}"
+#         f"&type={params['type']}"
+#         f"&key={api_key}"
+#         f"&opennow=true"
+#     )
+#     response = requests.get(url)
+
+#     if response.status_code == 200:
+#         possibleNearbyPlaces = response.json().get("results", [])
+#         all_reviews = []
+#         for place in possibleNearbyPlaces:
+#             reviews = get_reviews(place["place_id"])
+#             all_reviews.append(reviews)
+#         return all_reviews
+#     else:
+#         return []
