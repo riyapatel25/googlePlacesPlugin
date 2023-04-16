02d46c6338f2a580def3ae89cfe036ac7ad89e76
Shivani Patel <shivani@Shivanis-MacBook-Air.local>
2023-04-15 20:36:28 -0700

    started implementing plugin

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
 import requests
+<<<<<<< Updated upstream
+=======
+import os
+from waitress import serve
+
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
+CORS(app, origins=["http://127.0.0.1:5000", "http://localhost:5000", "https://chat.openai.com"])
+
 
 api_key = 'example_key
 '
@@ -18,9 +27,15 @@ def proccessPlaces():
     #iterate through all places and iterate through all its reviews to find best
         possibleNearbyPlaces = response.results
         all_reviews = []
+<<<<<<< Updated upstream
         for i, place in enumerate(possibleNearbyPlaces):
             reviews = get_reviews(place.place_id, api_key)
             all_reviews.push(reviews)
+=======
+        for place in possibleNearbyPlaces:
+            reviews = get_reviews(place["place_id"])
+            all_reviews.append(reviews)
+>>>>>>> Stashed changes
         return all_reviews
     else:
         return jsonify({"error": f"Error: {response.status_code}"}), response.status_code
@@ -34,5 +49,35 @@ def get_reviews(place_id, api_key):
     else:
         return jsonify({"error": f"Error: {response.status_code}"}), response.status_code
 
+<<<<<<< Updated upstream
 if __name__ == '__main__':
     app.run(debug=False)
+=======
+@app.route("/places", methods=["GET"])
+def get_place_reviews():
+    location = request.args.get("location")
+    radius = request.args.get("radius")
+    type_of = request.args.get("type")
+
+    try:
+        # Convert radius to integer
+        radius = int(radius)
+        reviews = proccessPlaces(location, radius, type_of)
+        # Return a JSON response
+        return jsonify({"reviews": reviews})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route("/.well-known/ai-plugin.json")
+def serve_ai_plugin():
+    return send_from_directory(".", "ai-plugin.json", mimetype="application/json")
+
+
+@app.route("/.well-known/openapi.yaml")
+def serve_openapi_yaml():
+    return send_from_directory(".", "openapi.yaml", mimetype="text/yaml")
+
+
+if __name__ == "__main__":
+    serve(app, host="127.0.0.1", port=5000)
+>>>>>>> Stashed changes
